42e8607d836e6989a9b1da64d0c2cbc3bcb40afd
yuma <164658568+vertexg@users.noreply.github.com>
2024-04-20 16:09:17 +0900

    Update reverseString.py

--- Recursion/reverseString.py
+++ Recursion/reverseString.py
@@ -8,3 +8,26 @@ def reverseStringHelper(reversedString, index, originalString) :
     return reverseStringHelper(originalString[index] + reversedString, index + 1, originalString)
 
 print(reverseString("recursion")) # noisrucer
+
+
+def reverseString(s):
+  return reverseStringHelper(s, "")
+
+def reverseStringHelper(s,string):
+  if len(s)<= 0:
+    return string
+  return reverseStringHelper(s[:-1], string + s[-1])
+
+print(reverseString("abcd"))
+
+
+
+s[:-1]とs[-1:]の違い
+
+s[:-1]:
+最初の要素から最後の要素の一つ前までを取り出すことができる
+例　s = "Hello"の場合、s[:-1]は"Hell"を返す
+
+s[-1:]:
+最後の要素のみを取り出す。
+例　s = "Hello"の場合、s[-1:]は"o"を返す
