fce9787d99ff3912e7403a33c15c876f2e6b2e99
yuma <164658568+vertexg@users.noreply.github.com>
2024-05-11 07:39:50 +0900

    Update trumpGame.py

--- List/trumpGame.py
+++ List/trumpGame.py
@@ -1,91 +1,97 @@
 import random
+
 class Card:
-    def __init__(self, value, suit, intValue) :
-      self.suit = suit
-      self.intValue = intValue
-      self.value = value
+    def __init__(self, value, suit, intValue):
+        self.value = value
+        self.suit = suit
+        self.intValue = intValue
+
     def getCardString(self):
-      return self.suit  + self.value +"(" + str(int(self.intValue)) + ")"
+        return self.suit + self.value + "(" + str(self.intValue) + ")"
 
 class Deck:
-    def __init__(self):
-      self.deck = self.generateDeck()
+    def __init__(self, gameMode=None):
+        self.deck = self.generateDeck(gameMode)
 
     @staticmethod
-    def generateDeck():
-      newDeck = []
-      values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-      suits = ["♣", "♦", "♥", "♠"]
+    def generateDeck(gameMode=None):
+        newDeck = []
+        values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        blackJack = {"A":1, "J":10,"Q":10,"K":10}
+        suits = ["♣", "♦", "♥", "♠"]
 
-      for suit in suits:
-        for i , value in enumerate(values):
-           newDeck.append(Card(value, suit, i + 1))
-      return newDeck
+        for suit in suits:
+            for i, value in enumerate(values):
+                intValue = (blackJack[value] if value in blackJack.keys() else int(value)) if gameMode == "21" else (i+1)
+                newDeck.append(Card(value, suit, intValue))
+
+        return newDeck
+
+    def draw(self):
+        return self.deck.pop()
 
     def printDeck(self):
-     print("Displaying cards...")
-     for i in self.deck:
-        print(i.getCardString())
+        print("Displaying cards...")
+        for card in self.deck:
+            print(card.getCardString())
 
     def shuffleDeck(self):
-       size = len(self.deck)
-       for i in range(size - 1, 0, -1):
-            j = random.randint(i, size -1)
+        deckSize = len(self.deck)
+        for i in range(deckSize - 1, 0, -1):
+            j = random.randint(i, deckSize-1)
             temp = self.deck[i]
             self.deck[i] = self.deck[j]
             self.deck[j] = temp
 
-    def draw(self):
-        return self.deck.pop()
 class Dealer:
 
     @staticmethod
-    def startGmae (amountOfPlayers, gamemode):
+    def startGame(amountOfPlayers, gameMode):
         table = {
             "players": [],
-            "gamemod":gamemode,
-            "deck": Deck(),
+            "gameMode": gameMode,
+            "deck": Deck(gameMode)
         }
 
         table["deck"].shuffleDeck()
 
         for person in range(0, amountOfPlayers):
-            # プレーヤの手札
             playerCard = []
-            for i in range (0, Dealer.initialCards(gamemode)):
+            for i in range (0, Dealer.initialCards(gameMode)):
                 playerCard.append(table["deck"].draw())
             table["players"].append(playerCard)
 
         return table
+
     @staticmethod
-    def initialCards(gamemode):
-       if gamemode == "21":
-          return 2
-       if gamemode == "poker":
-          return 5
+    def initialCards(gameMode):
+        if gameMode == "21":
+            return 2
+        if gameMode == "poker":
+            return 5
+
     @staticmethod
     def printTableInformation(table):
-       print("Amount of players: " + str(len(table["players"])) + "... Game mode: " + table["gamemod"] + ". At this table: ")
+        print("Amount of players: " + str(len(table["players"])) + "... Game mode: " + table["gameMode"] + ". At this table: ")
 
-       for i, player in enumerate(table["players"]):
-         print(str(i + 1) + "player cards")
-         for card in player:
-            print(card.getCardString())
+        for i, player in enumerate(table["players"]):
+            print(str(i + 1) + "player's cards: ")
+            for card in player:
+                print(card.getCardString())
+
     @staticmethod
     def score21Individual(cards):
-          vaule = 0
-          for card in cards:
-              vaule += card.intValue
-          return vaule if 21 >= vaule >= 1 else 0
+        value = 0
+        for card in cards:
+            value += card.intValue
+        return value if 21 >= value >= 1 else 0
 
- @staticmethod
-  
+    @staticmethod
     def winnerOf21(table):
         points = []
         cache = {}
         for cards in table["players"]:
             point = Dealer.score21Individual(cards)
-            # pointを配列に保存
             points.append(point)
             if point in cache:
                 cache[point] += 1
@@ -102,6 +108,14 @@ class Dealer:
         else:
             return "No winners.."
 
+    # 卓のゲームの種類によって勝利条件を変更するcheckWinnerというメソッドを作成します。
+    @staticmethod
+    def checkWinner(table):
+        if table["gameMode"] == "21":
+            return Dealer.winnerOf21(table)
+        else:
+            return "no game"
+
 class HelperFunctions:
 
     @staticmethod
@@ -115,6 +129,10 @@ class HelperFunctions:
 
         return maxIndex
 
-table = Dealer.startGame(4, "21")
-Dealer.printTableInformation(table)
-print(Dealer.winnerOf21(table))
+table1 = Dealer.startGame(2, "21")
+Dealer.printTableInformation(table1)
+print(Dealer.checkWinner(table1))
+
+table2 = Dealer.startGame(1, "poker")
+Dealer.printTableInformation(table2)
+print(Dealer.checkWinner(table2))
