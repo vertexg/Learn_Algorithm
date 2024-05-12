0fb6375b8d2ffd188788dbc1efd46385b443dfd2
yuma <164658568+vertexg@users.noreply.github.com>
2024-05-12 11:02:38 +0900

    Update PascalsTriangle.py

--- Recursion/PascalsTriangle.py
+++ Recursion/PascalsTriangle.py
@@ -3,3 +3,14 @@ def numberOfDots(x):
         return 1
       
     return numberOfDots(x - 1) + x
+
+
+``` java
+class Solution{
+    public static int numberOfDots(int x){
+        if (x <= 0) return 0;
+       return x + numberOfDots(x-1);
+        
+    }
+}
+```
