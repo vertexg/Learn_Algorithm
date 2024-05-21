3af7fb05960996e0621538809e992d5a3471114e
yuma <164658568+vertexg@users.noreply.github.com>
2024-05-21 23:12:31 +0900

    Create perfectNumberList.java

---
+++ ControlFlow/perfectNumberList.java
@@ -0,0 +1,23 @@
+class Solution {
+  public static String perfectNumberList(int n) {
+    String output = "";
+
+    for (int i = 1; i <= n; i++) {
+      if (isPerfectNumber(i)) {
+        output += i + "-";
+      }
+    }
+    if (output == "")return "none";
+    return output.substring(0, output.length() - 1);
+  }
+
+  public static boolean isPerfectNumber(int n) {
+    int sum = 0;
+    for (int i = 1; i < n; i++) {
+      if (n % i == 0) {
+        sum += i;
+      }
+    }
+    return sum == n;
+  }
+}
